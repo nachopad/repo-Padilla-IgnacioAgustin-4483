b784b4a39f75be2fb25f4b94d65b033cd10461c9
nachopad <nacho20002008@gmail.com>
2022-04-06 23:20:31 -0300

    primer commit

---
+++ ProjectDemo/src/test/java/ar/edu/fi/unju/aplicacion/ejemplos/ProjectDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.fi.unju.aplicacion.ejemplos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
